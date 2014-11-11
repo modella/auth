309a9f4c950f27db0f40ec4ca27cef45a1745868
Matthew Mueller <mattmuelle@gmail.com>
2014-11-11 02:13:40 -0800

    make more generic

--- index.js
+++ index.js
@@ -7,40 +7,50 @@ var pwd = require('pwd');
 /**
  * Expose `model-auth`
  *
- * @param {Model} User
- */
-
-module.exports = function(User) {
-  User.attr('salt');
-  User.on('saving', function(user, done) {
-    if(user.salt()) return done();
-    pwd.hash(user.password(), function(err, salt, hash) {
-      user.salt(salt);
-      user.password(hash);
-      done();
-    });
-  });
-
-  User.authorize = authorize.bind(User);
-};
-
-/**
- * Authorize
+ * @param {String} prop
  *
- * @param {String} email
- * @param {String} password
- * @param {Function} fn
+ * @param {Model} model
  */
 
-function authorize(email, password, fn) {
-  this.get({ email : email }, function(err, user) {
-    if(err || !user) return fn(err, user);
-    pwd.hash(password, user.salt(), function(err, hash) {
-      if(user.password() == hash) {
-        return fn(null, user);
-      } else {
-        return fn(null, false);
-      }
+module.exports = function(opts) {
+  opts = opts || {};
+  var password = opts.password || 'password';
+  var salt = opts.salt || 'salt';
+
+  return function(Model) {
+    // attr
+    Model.attr(salt);
+
+    // on saving
+    Model.on('saving', function(model, done) {
+      if(model[salt]()) return done();
+      pwd.hash(model[password](), function(err, s, hash) {
+        model[salt](s);
+        model[password](hash);
+        done();
+      });
     });
-  });
-}
+
+    /**
+     * Model.authorize
+     *
+     * @param {String} query
+     * @param {String} password
+     * @param {Function} fn
+     */
+
+    Model.authorize = function authorize(query, pass, fn) {
+      Model.find(query, function(err, model) {
+        if(err || !model) return fn(err, model);
+        pwd.hash(pass, model[salt](), function(err, hash) {
+          if(model[password]() == hash) {
+            return fn(null, model);
+          } else {
+            return fn(null, false);
+          }
+        });
+      });
+    };
+
+  }
+};
